8eb5499e5fc7db08744dfab124828373f8af4001
Mahmoud Abdulmawlaa <116794637+mahmoudabusalem@users.noreply.github.com>
2024-10-16 21:21:31 +0300

    Week3-Day3

---
+++ Week-3/Day-3/Lab/Program.cs
@@ -0,0 +1,80 @@
+﻿using Time;
+
+namespace Lab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //-----------------------------------------------------------------------------------
+
+            Duration D1 = new Duration(10, 15, 30);
+            Duration D2 = new Duration(16, 22, 6);
+
+            //-----------------------------------------------------------------------------------
+
+            Console.WriteLine($"D1 = {D1}");
+            Console.WriteLine($"D2 = {D2}");
+
+            Duration D3 = D1 + D2;
+
+            Console.WriteLine($"D3 = D1 + D2 = {D3}");
+
+            Console.WriteLine();
+
+            //-----------------------------------------------------------------------------------
+
+            Console.WriteLine($"D1 = {D1}");
+            Console.WriteLine($"D2 = {D2}");
+            Console.WriteLine($"D3 = {D3}");
+
+            D3 = 666 + D3;
+
+            Console.WriteLine($"666 + D3 = {D3}");
+
+            Console.WriteLine();
+
+            //-----------------------------------------------------------------------------------
+
+            Console.WriteLine($"D1 = {D1}");
+            Console.WriteLine($"D2 = {D2}");
+            Console.WriteLine($"D3 = {D3}");
+
+            D3++;
+
+            Console.WriteLine($"D3++ = {D3}");
+
+            Console.WriteLine();
+
+            //-----------------------------------------------------------------------------------
+
+            Console.WriteLine($"D1 = {D1}");
+            Console.WriteLine($"D2 = {D2}");
+            Console.WriteLine($"D3 = {D3}");
+
+            D3 = --D2;
+
+            Console.WriteLine($"D3 = --D2 = {D3}");
+
+            Console.WriteLine();
+
+            //-----------------------------------------------------------------------------------
+
+            Console.WriteLine($"D1 = {D1}");
+            Console.WriteLine($"D2 = {D2}");
+
+            if (D1 <= D2)
+            {
+                Console.WriteLine("D1 is less than or equal to D2");
+            }
+            else
+            {
+                Console.WriteLine("D1 is greater than D2");
+            }
+
+            Console.WriteLine();
+
+            //-----------------------------------------------------------------------------------
+        }
+    }
+}
---
+++ Week-3/Day-3/Time/Time.cs
@@ -0,0 +1,98 @@
+﻿namespace Time
+{
+    public class Duration
+    {
+        public int Hours { get; set; }
+        public int Minutes { get; set; }
+        public int Seconds { get; set; }
+
+        public Duration(int hours, int minutes, int seconds)
+        {
+            Hours = hours;
+            Minutes = minutes + seconds / 60;
+            Seconds = seconds % 60;
+        }
+
+        public Duration(int seconds)
+        {
+            Hours = seconds / 3600;
+            Minutes = (seconds % 3600) / 60;
+            Seconds = seconds % 60;
+        }
+
+        public override string ToString()
+        {
+            return $"{Hours}H:{Minutes}M:{Seconds}S";
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is Duration duration)
+            {
+                return Hours == duration.Hours && Minutes == duration.Minutes && Seconds == duration.Seconds;
+            }
+
+            return false;
+        }
+
+        public static Duration operator +(Duration duration1, Duration duration2)
+        {
+            return new Duration(duration1.Hours + duration2.Hours, duration1.Minutes + duration2.Minutes, duration1.Seconds + duration2.Seconds);
+        }
+
+        public static Duration operator +(int seconds, Duration duration)
+        {
+            return new Duration(duration.Hours, duration.Minutes, duration.Seconds + seconds);
+        }
+
+        public static Duration operator -(Duration duration1, Duration duration2)
+        {
+            return new Duration(duration1.Hours - duration2.Hours, duration1.Minutes - duration2.Minutes, duration1.Seconds - duration2.Seconds);
+        }
+
+        public static Duration operator -(int seconds, Duration duration)
+        {
+            return new Duration(duration.Hours, duration.Minutes, duration.Seconds - seconds);
+        }
+
+        public static bool operator ==(Duration duration1, Duration duration2)
+        {
+            return duration1.Equals(duration2);
+        }
+
+        public static bool operator !=(Duration duration1, Duration duration2)
+        {
+            return !duration1.Equals(duration2);
+        }
+
+        public static bool operator >(Duration duration1, Duration duration2)
+        {
+            return duration1.Hours > duration2.Hours || duration1.Hours == duration2.Hours && (duration1.Minutes > duration2.Minutes || duration1.Minutes == duration2.Minutes && duration1.Seconds > duration2.Seconds);
+        }
+
+        public static bool operator <(Duration duration1, Duration duration2)
+        {
+            return duration1.Hours < duration2.Hours || duration1.Hours == duration2.Hours && (duration1.Minutes < duration2.Minutes || duration1.Minutes == duration2.Minutes && duration1.Seconds < duration2.Seconds);
+        }
+
+        public static bool operator >=(Duration duration1, Duration duration2)
+        {
+            return duration1.Hours > duration2.Hours || duration1.Hours == duration2.Hours && (duration1.Minutes > duration2.Minutes || duration1.Minutes == duration2.Minutes && duration1.Seconds >= duration2.Seconds);
+        }
+
+        public static bool operator <=(Duration duration1, Duration duration2)
+        {
+            return duration1.Hours < duration2.Hours || duration1.Hours == duration2.Hours && (duration1.Minutes < duration2.Minutes || duration1.Minutes == duration2.Minutes && duration1.Seconds <= duration2.Seconds);
+        }
+
+        public static Duration operator ++(Duration duration)
+        {
+            return new Duration(duration.Hours, duration.Minutes + 1, duration.Seconds);
+        }
+
+        public static Duration operator --(Duration duration)
+        {
+            return new Duration(duration.Hours, duration.Minutes - 1, duration.Seconds);
+        }
+    }
+}
